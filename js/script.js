ae94d7a3d020a5ce149d1cd413545c44539b6593
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-11 16:51:17 +0200

    working code needs styles

--- js/script.js
+++ js/script.js
@@ -28,13 +28,133 @@ console.log (welcomeBtn);
 const gameBtn = document.querySelector('.play-game-btn');
 console.log(gameBtn);
 
-const loginBtn = document.querySelector('.accedi-js')
-console.log(loginBtn);
+const loginBtnMain = document.querySelector('.accedi-main');
+console.log(loginBtnMain);
 
-let logged = false;
+const notLoggedContainer = document.querySelector('.notlogged');
+console.log(notLoggedContainer);
+
+const loginContainer = document.querySelector('.login-container');
+console.log(loginContainer);
+
+const navLoginBtn = document.querySelector('.accedi-nav');
+
+const navLogoutBtn = document.querySelector('.logout-nav');
+
+
+let logged = true;
 console.log(logged);
 
+const submitBtn = document.querySelector('.submit-fake');
+console.log(submitBtn)
+const mailAcc = ["a@gmail.com", "b@gmail.com", "c@gmail.com"]
 
-loginBtn.addEventListener('click', function(){
-    
+let mailValue = document.querySelector('.email').value;
+
+const headernotlogged = document.querySelector('.notlogged>h1');
+
+const header3notlogged = document.querySelector('.notlogged>h3');
+
+const gameMainBtn = document.querySelector('.gioca-main');
+const gameNavBtn = document.querySelector('.play-game-btn');
+
+const gameSection = document.querySelector('.game');
+
+loginBtnMain.addEventListener('click', function(){
+    notLoggedContainer.classList.toggle('display-none');
+    loginContainer.classList.toggle('display-none');
+})
+
+welcomeBtn.addEventListener('click', function(){
+    if(!(logged)){
+        notLoggedContainer.classList.remove('display-none');
+        loginContainer.classList.add('display-none');
+    } else{
+        gameSection.classList.add('display-none');
+        notLoggedContainer.classList.remove('display-none');
+
+        headernotlogged.innerHTML = `Loggato vai a giocare!`;
+
+        header3notlogged.innerHTML = "Schiaccia gioca per giocare";
+
+        loginBtnMain.classList.add('display-none');
+
+        console.log(gameMainBtn)
+        gameMainBtn.classList.remove('display-none');
+        
+        
+    }
+})
+
+submitBtn.addEventListener('click', function test(){
+
+    for(i = 0; i < mailAcc.length; i++){
+        mailAcc[i];
+        console.log(mailAcc[i])
+        // const errormessage = document.querySelector('.errorP')
+        if(mailAcc[i] === mailValue ){
+            logged = true;
+            console.log(logged);
+            alert("loggato");
+
+            navLoginBtn.classList.add('display-none')
+
+            loginContainer.classList.add('display-none');
+
+            notLoggedContainer.classList.remove('display-none')
+            headernotlogged.innerHTML = `Loggato vai a giocare!`;
+
+            header3notlogged.innerHTML = "Schiaccia gioca per giocare";
+
+            loginBtnMain.classList.add('display-none');
+
+            navLogoutBtn.classList.remove('display-none');
+
+
+            gameMainBtn.classList.remove('display-none');
+        } 
+        // else if (!(errormessage) || (mailAcc === mailValue) ){
+        //     loginContainer.innerHTML += `<p class="errorP" style="color:red;">Email sbagliata o non registrata</p>`
+        // }
+    }
+})
+
+navLogoutBtn.addEventListener('click', function(){
+    location.reload();
+})
+
+
+gameMainBtn.addEventListener('click', function(){
+    notLoggedContainer.classList.add('display-none');
+    gameSection.classList.remove('display-none');
+})
+
+gameNavBtn.addEventListener('click', function(){
+    notLoggedContainer.classList.add('display-none');
+    gameSection.classList.remove('display-none');
+})
+
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const startGame = document.querySelector('.lancia');
+console.log(startGame)
+
+startGame.addEventListener('click',function(){
+    // console.log(Math.ceil(Math.random()*6))
+    let playerNumber = Math.ceil(Math.random()*6)
+    console.log("player", playerNumber)
+
+    let pcNumber = Math.ceil(Math.random()*6)
+    console.log("ps", pcNumber)
+
+    if(playerNumber == pcNumber){
+        console.log("avete pareggiato");
+    } else if(playerNumber > pcNumber){
+        console.log("player ha vinto");
+    } else{
+        console.log("pc ha vinto")
+    }
 })
